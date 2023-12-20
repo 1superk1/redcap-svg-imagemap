2f531255c6e10732f1019cda7c0c7ba9ab2d33b1
1superk1 <guykelman@gmail.com>
2023-12-20 15:07:27 +0200

    Added the option to embed the svg-imagemap to a display field
    Committer: @1superk1
    
    on branch: add-embedding-field
    
    modified: svgMap.js
    using the DOM to search for the svg image. It might be embedded inside a display field. In that case, we search for the original choice field (where the checkboxes/radio are) in order to crank up the onClick triggers.

--- svgMap.js
+++ svgMap.js
@@ -1,6 +1,15 @@
+/*
+ *
+ * MODIFIED
+ *    $Date: Sun Nov 19 15:02:14 IST 2023$
+ *
+ */
+
 $(document).ready(function() {
     $.each(UIOWA_SvgMap.embedFields, function(index, field) {
-        let $embedField = $("[sq_id='" + field + "'] > td");
+        let $embedField = $("[sq_id='" + field + "'] > td > div[data-kind='field-label']  > div");
+	if ($embedField.length === 0)
+	    $embedField = $("[var='" + field + "'] div[data-kind='field-label'] div[data-mlm-field='" + field + "']")
 
         // if embed target field exists on this form, embed SVG
         if ($embedField.length > 0) {
@@ -19,12 +28,32 @@ $(document).ready(function() {
         this.addEventListener('load', function() {
             let $svg = $(this);
             let $contents = $svg.contents();
+
+            // locate the choice field. If it is suffixed with '-tr'
+	        // it means that the svg image was not embedded inside
+	        // another display field
             let $choiceField = $('#' + $svg.attr('svg-choice-field') + '-tr');
 
             // update field td to fit loaded SVG
             $svg.parent().height($contents.height());
+	        $svg.parent().width($contents.width());
 
-            $choiceField.find('.data input')
+            //$choiceField.find('.data input')
+	        let $data_cls = ".data";
+	        let $hidden_cls = [];
+     	    try {
+                // split-filter of the class attribute to locate a
+		        // possible 'hide' class. This is inside a try-catch
+		        // to avoid errors when $hidden_cls is empty
+        	    $hidden_cls = $choiceField.attr('class').split(/\s+/).filter( function(e) { return e === 'hide' } );
+      	    }
+     	    catch(e) {  /* console.log(e)  */  };
+
+            if ($hidden_cls.length > 0) {       // field embedding took place. Update the choice variable in its original location
+                   $choiceField = $('.rc-field-embed[var="' + $svg.attr('svg-choice-field') + '"]');   // update the choice variable
+                   $data_cls = "";     // no data class in the current choice variable
+            }
+            $choiceField.find($data_cls + ' input')
                 // update map if input clicked
                 .on('click', function() {
                     updateMap($svg, this);
